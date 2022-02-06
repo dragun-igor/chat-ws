11a8a8cf00dec62f8cba8b415e17892c18336520
Igor Dragun <dragun.msc@gmail.com>
2022-02-06 13:04:18 +0300

    fix

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -109,6 +109,12 @@ func ListenToWsChannel() {
 			response.Action = "list_users"
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+		case "left":
+			response.Action = "list_users"
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadcastToAll(response)
 		}
 
 		//response.Action = "Got here"
@@ -120,7 +126,9 @@ func ListenToWsChannel() {
 func getUserList() []string {
 	var userList []string
 	for _, x := range clients {
-		userList = append(userList, x)
+		if x != "" {
+			userList = append(userList, x)
+		}
 	}
 	sort.Strings(userList)
 	return userList
